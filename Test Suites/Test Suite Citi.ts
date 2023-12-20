cffade116f1593e523f667a1766935b59682c78a
mdeleont <mdeleont@LMX-5CG1515B6N.corp.capgemini.com>
2023-12-19 20:22:50 -0600

    Test suite

---
+++ Test Suites/Test Suite Citi.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite Citi</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>09626c36-98d2-46ce-9d8f-3e16fccefe7f</testSuiteGuid>
+   <testCaseLink>
+      <guid>0ceae0b7-d8dd-4a28-bc84-0d08b9c42789</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Solicitar tarjeta/Solicitar tarjeta</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
