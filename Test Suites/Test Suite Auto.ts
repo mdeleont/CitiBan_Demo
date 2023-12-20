0a0ee1fab089dd1b5a8febe659408006bdeff0ad
mdeleont <mdeleont@LMX-5CG1515B6N.corp.capgemini.com>
2023-12-19 20:27:05 -0600

    Test collections

---
+++ Test Suites/Test Suite Auto.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite Auto</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>fe10c7f3-fc1b-4896-ba8f-a415c8e12bda</testSuiteGuid>
+   <testCaseLink>
+      <guid>78f14756-b26b-45c1-afd6-848467ffe88c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Seguro de auto/Cotizar Seguro de auto</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
